a83dbeca0ded764580a79112bde782e1dbd47be5
WesleyLopezHudson <wesleylopezhudson@gmail.com>
2025-10-23 23:26:58 +0000

    Added last part that plays the command and changed Test_even to test_even

--- problem_3.py
+++ problem_3.py
@@ -1,13 +1,12 @@
 import unittest #self.assert functions
 
 def even_or_not(n): #need function to check if number is even or odd
-    return n % 2 == 1 #number over 2 tells if even or odd
+    return n % 2 == 0 #number over 2 tells if even or odd
 
 class Even_four(unittest.TestCase): 
 
-    def Test_even(self):   # define number
-        self.assertTrue(even_or_not(4)) #True or false + our variable, 4
+    def test_even(self):# define number and test has to be lowercase
+        self.assertTrue(even_or_not(4)) #True or false + the variable 4
 
-#if __name__ == '__problem.3__'
-#    unittest()
-#DNF
+if __name__ == '__main__': #This plays the command
+    unittest.main()
