5be14cff8a37137ea759a6ab13e7e02bd076a652
WesleyLopezHudson <wesleylopezhudson@gmail.com>
2025-10-23 23:11:13 +0000

    problem 3

--- problem_3.py
+++ problem_3.py
@@ -1,1 +1,13 @@
+import unittest #self.assert functions
 
+def even_or_not(n): #need function to check if number is even or odd
+    return n % 2 == 1 #number over 2 tells if even or odd
+
+class Even_four(unittest.TestCase): 
+
+    def Test_even(self):   # define number
+        self.assertTrue(even_or_not(4)) #True or false + our variable, 4
+
+#if __name__ == '__problem.3__'
+#    unittest()
+#DNF
