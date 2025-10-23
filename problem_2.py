bb4d1c732d421e0cf9719d464db1d75c9de23dc1
VicktoriaL692 <vicktorialee6@gmail.com>
2025-10-23 18:28:05 -0400

    Update problem_2.py
    Calculates the factorial of an input

--- problem_2.py
+++ problem_2.py
@@ -1,1 +1,13 @@
+#File 2 (problem2.py): Write a function to calculate the factorial of a number.
 
+#Example:
+#Input: 5
+#Output: 120
+
+def factorial(n):
+   if n == 0: 
+      return 1
+   else: 
+      return n * factorial(n - 1)
+
+print(factorial(5)) # Output: 120
